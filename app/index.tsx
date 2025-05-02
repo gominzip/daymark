08c3b2952eb1686c0ffbd35a872c7338e12d87d8
gominzip <ahaminji@naver.com>
2025-05-02 16:44:46 +0900

    ➕ Install NativeWind

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function MainScreen() {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-5xl text-red-800 font-bold">Hello World!</Text>
+    </View>
+  );
+}
